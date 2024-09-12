e1dd3dd375337dd2d6558ef727baca141a9a27dd
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-09-12 19:12:30 +0600

    Resize issue fixed

--- script.js
+++ script.js
@@ -13,13 +13,32 @@
 //     }
 // }).mount();
 
+let clickCount = 0;
 document.querySelector(".navbar__button--mobile-nav").addEventListener("click", function (e) {
     console.log("Hello");
+    clickCount++;
     document.querySelector(".navbar__mobile-view").classList.toggle("navbar__mobile-view--active");
     document.querySelector(".navbar__mobile-view").classList.toggle("navbar__mobile-view--inactive");
 });
 
+
 window.addEventListener("resize", function (e) {
-    document.querySelector(".navbar__mobile-view").classList.remove("navbar__mobile-view--active");
-    document.querySelector(".navbar__mobile-view").classList.add("navbar__mobile-view--inactive");
+    let width = window.innerWidth;
+    console.log(width);
+
+    if(width >= 768)
+    {
+        document.querySelector(".navbar__mobile-view").classList.remove("navbar__mobile-view--active");
+        document.querySelector(".navbar__mobile-view").classList.add("navbar__mobile-view--inactive");
+    }
+    else if(clickCount % 2 == 1) // if the mobile view was active
+    {
+        document.querySelector(".navbar__mobile-view").classList.remove("navbar__mobile-view--inactive");
+        document.querySelector(".navbar__mobile-view").classList.add("navbar__mobile-view--active");
+    }
+    else
+    {
+        document.querySelector(".navbar__mobile-view").classList.remove("navbar__mobile-view--active");
+        document.querySelector(".navbar__mobile-view").classList.add("navbar__mobile-view--inactive");
+    }
 });

ac3b74f38b3f72af217d648bb9b10b6568d30fe3
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-09-12 12:37:30 +0600

    Added Mobile Nav on click

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // import Splide from '@splidejs/splide';
 // new Splide( '.splide' ).mount();
-new Splide('.splide', {
-    type       : 'loop',
-    perPage    : 3,
-    perMove    : 1,
-    focus      : 'center',
-    pagination : false,
-    breakpoints: {
-        950: {
-            perPage: 1,
-        }
-    }
-}).mount();
+// new Splide('.splide', {
+//     type       : 'loop',
+//     perPage    : 3,
+//     perMove    : 1,
+//     focus      : 'center',
+//     pagination : false,
+//     breakpoints: {
+//         950: {
+//             perPage: 1,
+//         }
+//     }
+// }).mount();
+
+document.querySelector(".navbar__button--mobile-nav").addEventListener("click", function (e) {
+    console.log("Hello");
+    document.querySelector(".navbar__mobile-view").classList.toggle("navbar__mobile-view--active");
+    document.querySelector(".navbar__mobile-view").classList.toggle("navbar__mobile-view--inactive");
+});
+
+window.addEventListener("resize", function (e) {
+    document.querySelector(".navbar__mobile-view").classList.remove("navbar__mobile-view--active");
+    document.querySelector(".navbar__mobile-view").classList.add("navbar__mobile-view--inactive");
+});
